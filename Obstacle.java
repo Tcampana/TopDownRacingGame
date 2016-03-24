0e7332ca89bba6331e17c642f239848c6f72d1b9
Thomas Campana <tccampana@comcast.net>
2016-03-23 21:28:23 -0600

    Update Obstacle.java

--- Obstacle.java
+++ Obstacle.java
@@ -3,7 +3,7 @@ public class Obstacle
 {
   //code to randomly generate an obstacle
   private boolean randomGen;
-  //code to slow the road
+  //code to slow the road if the car hits the obstacle
   private int slowing;
   
   public Obstacle(boolean randomGen, int slowing)
